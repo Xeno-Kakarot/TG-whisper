697a36e55abd3df23dcf7baa624fae656cd749a8
dilshadhasnain07 <161922960+dilshadhasnain07@users.noreply.github.com>
2024-03-18 08:35:08 +0545

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,29 @@
+# https://github.com/dilshadhasnain07/TG-whisper
+# https://github.com/Team-ProjectCodeX
+
+
+class Config(object):
+    # Configuration class for the bot
+
+    # Enable or disable logging
+    LOGGER = True
+
+    # <================================================ REQUIRED ======================================================>
+    # Telegram API configuration
+    API_ID =24683098   # Get this value from my.telegram.org/apps
+    API_HASH = "e4055cd239464e50e69bd73057c05dd3"
+
+
+    # Support chat and support ID
+    SUPPORT_CHAT = "SYKA_SELLERS7"
+    SUPPORT_ID = -1002074034670
+
+    # Database name
+    DB_NAME = "MikoDB"
+
+    # Bot token
+    TOKEN = "6707007864:AAH_QTcljrDna_2EsN3MjW8tVYatwtY1s4s"  # Get bot token from @BotFather on Telegram
+
+    # Owner's Telegram user ID (Must be an integer)
+    OWNER_ID = 6346273488
+    # <=======================================================================================================>
